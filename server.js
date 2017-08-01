15a0969a8fbd3a0c508bb07a8391963c30002173
Cyrille Tuzi <web@cyrilletuzi.com>
2017-08-01 16:24:05 +0200

    (fix) easier routing

--- server.js
+++ server.js
@@ -6,22 +6,8 @@ const express = require('express');
 const ngUniversal = require('@nguniversal/express-engine');
 const appServer = require('./dist-server/main.bundle');
 
-function angularRouter(req, res) {
-
-  res.render('index', {
-    req,
-    res,
-    providers: [{
-      provide: 'serverUrl',
-      useValue: `${req.protocol}://${req.get('host')}`
-    }]
-  });
-
-}
-
 const app = express();
 
-app.get('/', angularRouter);
 app.use(express.static(`${__dirname}/dist`));
 
 app.get('/api', (req, res) => {
@@ -35,7 +21,18 @@ app.engine('html', ngUniversal.ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', 'dist');
 
-app.get('*', angularRouter);
+app.get('*', (req, res) => {
+
+  res.render('index', {
+    req,
+    res,
+    providers: [{
+      provide: 'serverUrl',
+      useValue: `${req.protocol}://${req.get('host')}`
+    }]
+  });
+
+});
 
 app.listen(3000, () => {
   console.log(`Listening on http://localhost:3000`);
